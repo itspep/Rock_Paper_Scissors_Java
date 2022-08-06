77b19f392a8cd01c42a12e3bc96077f06c0c7fec
Mumuni Tahiru <peptahiru@gmail.com>
2022-08-06 18:25:33 +0000

    commit

--- TheGame.java
+++ TheGame.java
@@ -4,30 +4,33 @@ public class TheGame{
     public static void main(String[] args){
         int userChoice;
         int pcChoice;
-        System.out.println("This Game allows you to play a Rock Paper Scissors with the computer");
-        System.out.println("To make your choice enter 1:Rock 2:Papper 3:Scissors");
-                Scanner input=new Scanner(System.in);
-                userChoice=input.nextInt();
-                Random RandNum=new Random();
-                pcChoice= RandNum.nextInt(3)+1;
-                switch (pcChoice)
-                {
-                    case 1:
-                        System.out.println("Rock");
-                        break;
-                    case 2:
-                        System.out.println("Papper");
-                    case 3:
-                        System.out.println("Scissors");
-                }
-                if(userChoice==pcChoice)
-                {
-                    System.out.println("It is a draw");
-                }
-                else if(userChoice<pcChoice)
-                {
-                    System.out.println("")
-                }
-
+        String again;
+        do {
+            System.out.println("This Game allows you to play a Rock Paper Scissors with the computer");
+            System.out.println("To make your choice enter 1:Rock 2:Papper 3:Scissors");
+            Scanner input = new Scanner(System.in);
+            userChoice = input.nextInt();
+            Random RandNum = new Random();
+            pcChoice = RandNum.nextInt(3) + 1;
+            switch (pcChoice) {
+                case 1:
+                    System.out.println("Rock");
+                    break;
+                case 2:
+                    System.out.println("Papper");
+                case 3:
+                    System.out.println("Scissors");
+            }
+            if (userChoice == pcChoice) {
+                System.out.println("It is a draw");
+            } else if (userChoice < pcChoice) {
+                System.out.println("You lost!");
+            } else if (userChoice > pcChoice) {
+                System.out.println("Congratulations! You won.");
+            } else System.out.println("Please enter a Valid Selection of 1, 2 or 3");
+            Scanner read=new Scanner(System.in);
+            System.out.println("Do you want to play again? Y=Yes, N=No");
+            again=read.nextLine();
+        }while(again.equalsIgnoreCase("y"));
     }
 }
